003a3ee661c7bd07a1d8159bc3a5a55559c84dc4
Annapurna Nyshadham <annapurna.nyshadham@gmail.com>
2021-03-13 20:35:17 +0530

    Add hillnum.py

---
+++ hillnum.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#program to print the hill number for a given input
+
+num = input("Enter a number : ")
+
+def hillnum(num):
+    hill = [ ]
+    #defining range where [i-1] and [i+1] fall between 0 and len(num)-1
+    for i in range(1,len(num)-2):
+        if num[i]>num[i-1] and num[i]>num[i+1]:
+            hill.append(num[i])
+    return hill
+
+#termination condition if input has less than 3 digits since a 
+#minimum of 3 digits is required to form a hill
+if len(num)<3:
+    print ("number length too short")
+
+print (hillnum(num))
+
