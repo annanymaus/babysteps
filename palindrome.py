23c2a6c493d9f93810c99324a96a23c8c4589fd6
Annapurna Nyshadham <annapurna.nyshadham@gmail.com>
2021-03-15 02:28:34 +0530

    Remove unnecessary conditions, exits

--- palindrome.py
+++ palindrome.py
@@ -1,17 +1,11 @@
+#!/usr/bin/env python3
+
+#program to check if a given string is a palindrome
  
-
-import sys
-
 n = input ("Enter a string : ")
 
-
-for i in n:
-    if (i == 0):
-        sys.exit(1)
-    if (n[::1] == n[::-1]):
-        print ("The string is a palindrome")
-        sys.exit(2)
-    else:
-        print ("Not a palindrome")
-        sys.exit(3)
+if (n[::1] == n[::-1]):
+    print ("The string is a palindrome")   
+else:
+    print ("Not a palindrome")
     
