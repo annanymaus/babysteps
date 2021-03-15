9b75f104a1aff2a676d11ba43863576ac7fbd5bf
Annapurna Nyshadham <annapurna.nyshadham@gmail.com>
2021-03-15 14:36:15 +0530

    Add stringsum.py

---
+++ stringsum.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#program to calculate the sum of digits in string of space-separated numbers
+
+#take input from user and split it into a list of strings
+n = input("Enter numbers separated by spaces : ").split()
+
+#define sum and initialise to 0
+sum = 0
+
+for i in range(len(n)):
+    #convert each list element to int
+    n[i] = int(n[i])
+    #calculate sum
+    sum = sum + n[i]
+
+#print sum
+print(sum)
