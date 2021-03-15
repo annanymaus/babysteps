eb41d9b19cd9c1b4c6da1e2aeaa33eba1fcafd78
Annapurna Nyshadham <annapurna.nyshadham@gmail.com>
2021-03-15 14:55:37 +0530

    Modify to provide base check first, calc. later

--- factorialrec.py
+++ factorialrec.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
+#program to find factorial of a given number
+
+#take input from user
 n = input("Enter a number : ")
 
+#recursive func. to calculate factorial
 def fact(i):
-    if (int (i) >=2):
-        c = i * fact(i-1)
-        return c
-    else:
+    #base check for 0! and 1!
+    if (int (i) < 2):
         return 1
 
+    #calculation for numbers greater than/equal to 2    
+    c = i * fact(i-1)
+    return c
+
+#print the result
 print (fact(int (n)))
